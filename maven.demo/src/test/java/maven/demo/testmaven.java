ef1e65c27150bfa6d5ad727e23c889ac34654556
ravi <ravi@KALSDEV24.DRONA.COM>
2018-04-10 19:45:34 +0800

    Initial checkin of Maven with Git.

---
+++ maven.demo/src/test/java/maven/demo/testmaven.java
@@ -0,0 +1,14 @@
+package maven.demo;
+
+import org.testng.annotations.Test;
+
+public class testmaven 
+{
+	@Test
+	public void test1()
+	{
+		System.out.println("Welcome to Maven project");
+		
+	}
+
+}
